17f9af0e296cb9d8be5e332d531e5247b8226c93
Bartosz Bazański <bartosz.b@webinterpreter.com>
2017-01-17 13:39:22 +0100

    Apply functioning programing into the quiz game

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,9 @@
     var $start = document.getElementById("startButton");
     var $form = document.getElementById("answer");
     var $timer = document.getElementById("timer");
-    var questionCounter;
-    var score;
+    var options = [], button;
+    var question, questions;
+    var score = 0;
     var time;
     var interval;
 
@@ -23,9 +24,9 @@
         description: "How many super heroes can you name?",
         question: "What is the real name of ",
         questions: [
-            {question: "Superman", answer: "Clarke Kent"},
-            {question: "Batman", answer: "Bruce Wayne"},
-            {question: "Wonderwoman", answer: "Dianna Prince"}
+            {question: "Superman", answer: "Clarke Kent", "asked": false},
+            {question: "Batman", answer: "Bruce Wayne", "asked": false},
+            {question: "Wonderwoman", answer: "Dianna Prince", "asked": false}
         ]
     }
 
@@ -37,50 +38,89 @@
         element.style.display = "block";
     }
 
+    function random(a, b, callback) {
+        if(b === undefined) {
+            b = a, a = 1;
+        }
+        var result = Math.floor((b-a+1) * Math.random()) + a;
+        if(typeof callback === "function") {
+            result = callback(result);
+        }
+        return result;
+    }
+
     function play(quiz) {
         time = 20;
         update($timer, time);
         interval = window.setInterval(countDown, 1000);
-        questionCounter = 0;
         score = 0;
         update($score, score);
         hide($start);
         show($form);
         chooseQuestion();
-        $form.addEventListener("submit", onQuestionSubmit, false);
-    }
-    function onQuestionSubmit(event) {
-        event.preventDefault();
-        check($form[0].value);
+        $form.addEventListener("click", function(event) {
+            event.preventDefault();
+            check(event.target.value)
+        }, false);
     }
     function chooseQuestion() {
         console.log("chooseQuestion() invoked");
-        var question = quiz.questions[questionCounter].question;
+        questions = filterNotAskedQuestions();
+        question = questions[random(questions.length) - 1];
         ask(question);
     }
 
     function ask(question) {
         console.log("ask() invoked");
-        update($question, quiz.question + question + "?");
-        $form[0].value = "";
-        $form[0].focus();
+        question.asked = true;
+        update($question, quiz.question + question.question + "?");
+        var option1 = chooseOption();
+        options.push(option1.answer);
+        console.log(options);
+        var option2 = chooseOption();
+        options.push(option2.answer);
+        console.log(options);
+        options.splice(random(0, 2), 0, question.answer);
+        console.log(options);
+        options.forEach(function(name) {
+            button = document.createElement("button");
+            button.value = name;
+            button.textContent = name;
+            $form.appendChild(button);
+        })
+
+    }
+
+    function chooseOption() {
+        var option = quiz.questions[random(quiz.questions.length) - 1];
+        console.log("Option:", option);
+        console.log("Question:", question);
+        if(option === question || options.indexOf(option.answer) !== -1) {
+            return chooseOption();
+        }
+        return option;
     }
 
     function check(answer) {
         console.log("check() invoked");
-        if(answer === quiz.questions[questionCounter].answer) {
+        if(answer === question.answer) {
             update($feedback, "Correct!", "right");
             score ++;
             update($score, score);
         } else {
             update($feedback, "Wrong!", "wrong");
         }
-        questionCounter++;
-        if(questionCounter === quiz.questions.length) {
+        if(filterNotAskedQuestions().length === 0) {
             gameOver();
         } else {
             chooseQuestion();
         }
+    }
+
+    function filterNotAskedQuestions() {
+        return quiz.questions.filter(function(question) {
+            return question.asked === false;
+        });
     }
 
     function gameOver() {
@@ -88,8 +128,14 @@
         show($start);
         hide($form);
         update($question, "Game Over, you scored " + score + " points.")
-        $form.removeEventListener("submit", onQuestionSubmit, false);
         window.clearInterval(interval);
+        resetQuestionStatuses();
+    }
+
+    function resetQuestionStatuses() {
+        quiz.questions.forEach(function(question) {
+            question.asked = false;
+        });
     }
 
     function update(element, content, additionalClass) {
